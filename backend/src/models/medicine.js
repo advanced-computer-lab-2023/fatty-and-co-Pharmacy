a79c661ce3ea76374969eb153664ad4802f8548b
Zeyadptd <zeyad.habashy02@gmail.com>
2023-10-08 00:12:58 +0300

    changed medicine schema name

--- backend/src/models/medicine.js
+++ backend/src/models/medicine.js
@@ -1,39 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const medicineSchema = new Schema({
-  Name: {
-    type: String,
-    required: true,
+const medicineSchema = new Schema(
+  {
+    Name: {
+      type: String,
+      required: true,
+    },
+    Quantity: {
+      type: Number,
+      required: true,
+    },
+    Active_Ingredients: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    Description: {
+      type: String,
+    },
+    Price: {
+      type: Number,
+      required: true,
+    },
+    Image: {
+      type: String,
+    },
+    Medicinal_Use: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    Sales: {
+      type: Number,
+      default: 0,
+    },
   },
-  Quantity: {
-    type: Number,
-    required: true
-  },
-  Active_Ingredients:[{
-    type: String,
-    required: true,
-  }],
-  Description: {
-    type: String,
-  },
-  Price: {
-    type: Number,
-    required: true,
-  },
-  Image: {
-    type: String
-  },
-  Medicinal_Use:[{
-    type: String,
-    required: true,
-  }],
-  Sales: {
-    type: Number,
-    default: 0
-  }, 
-  
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-const Medicine = mongoose.model('medicineSchema', medicineSchema);
+const Medicine = mongoose.model("Medicine", medicineSchema);
 module.exports = Medicine;
