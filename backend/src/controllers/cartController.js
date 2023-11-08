01e22e26e506c815edd71f82fe79df91337c3fb3
mariamhmostafa <mariamhmaarek@gmail.com>
2023-11-08 17:47:41 +0200

    Add cartController file

---
+++ backend/src/controllers/cartController.js
@@ -0,0 +1,5 @@
+const patientModel = require("../models/patients");
+const orderModel = require("../models/orders");
+const cartModel = require("../models/cart");
+const medicineModel = require("../models/medicine");
+
