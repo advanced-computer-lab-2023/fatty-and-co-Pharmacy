50c575b101a41922bbe62fddefa4c0f8bff41c01
mariamhmostafa <mariamhmaarek@gmail.com>
2023-11-11 15:52:16 +0200

    Clean up cart and checkout views

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -3,56 +3,50 @@ const orderModel = require("../models/orders");
 const cartModel = require("../models/cart");
 
 const checkout = async (req, res) => {
-  try {
-    const username = req.user.Username;
-    const cart = await cartModel.findOne({ PatientUsername: username });
-    const patient = await patientModel.findOne({ PatientUsername: username });
-    const wallet = patient.Wallet;
-    const medicine = cart.Medicine;
-    const totalCost = cart.TotalCost;
-    console.log(cart);
+    try {
+        const username = req.user.Username;
+        const cart = await cartModel.findOne({ PatientUsername: username });
+        const patient = await patientModel.findOne({ Username: username });
+        console.log(patient);
+        console.log(username);
+        const wallet = patient.Wallet;
+        const medicine = cart.Medicine;
+        const totalCost = cart.TotalCost;
+        console.log(cart);
 
-    const paymentMethod = req.query.PaymentMethod;
-    const details = req.query.Details;
-    const deliveryAddress = req.query.DeliveryAddress;
-    if (paymentMethod == "Wallet") {
-      if (wallet < totalCost) {
-        res.status(400).send({ message: "YOU ARE POOR" });
-        return;
-      } else {
-        const patientnew = await patientModel.findOneAndUpdate(
-          {
+        const paymentMethod = req.query.PaymentMethod;
+        const details = req.query.Details;
+        const deliveryAddress = req.query.DeliveryAddress;
+        if (paymentMethod == "Wallet") {
+            if (wallet < totalCost) {
+                res.status(400).send({ message: "Insufficient wallet balance" });
+                return;
+            } else {
+                const newWallet = wallet - totalCost;
+                patient.Wallet = newWallet;
+                await patient.save();
+            }
+        }
+        const newOrder = await orderModel.create({
             PatientUsername: username,
-            Wallet: wallet - totalCost,
-          },
-          { new: true }
-        );
-      }
+            Date: new Date(),
+            Status: "Pending",
+            Medicine: medicine,
+            Details: details,
+            TotalCost: totalCost,
+            PaymentMethod: paymentMethod,
+            DeliveryAddress: deliveryAddress,
+        });
+        await newOrder.save();
+        cart.TotalCost = 0;
+        cart.Medicine = [];
+        await cart.save();
+        res.status(200).send(newOrder);
+    } catch (error) {
+        res.status(400).send({ message: error.message });
     }
-    const newOrder = await orderModel.create({
-      PatientUsername: username,
-      Date: new Date(),
-      Status: "Pending",
-      Medicine: medicine,
-      Details: details,
-      TotalCost: totalCost,
-      PaymentMethod: paymentMethod,
-      DeliveryAddress: deliveryAddress,
-    });
-    await newOrder.save();
-    const newmed = [];
-    const newcost = 0;
-    await cartModel.findOneAndUpdate({
-      PatientUsername: username,
-      Medicine: newmed,
-      TotalCost: newcost,
-    });
-    res.status(200).send({ message: newOrder });
-  } catch (error) {
-    res.status(400).send({ message: error.message });
-  }
 };
 
 module.exports = {
-  checkout,
+    checkout,
 };
