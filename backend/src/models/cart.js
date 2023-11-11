9369b2a41426566d16dedabf82ee013b1917c13a
Zeyadptd <zeyad.habashy02@gmail.com>
2023-11-11 19:06:35 +0200

    Refactor cart model schema to use object IDs for medicine field

--- backend/src/models/cart.js
+++ backend/src/models/cart.js
@@ -2,20 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const cartSchema = new Schema({
-    PatientUsername: {
-        type: String,
-        ref: "Patient",
-        required: true,
-        trim: true,
-    },
-    TotalCost: {
-        type: Number,
-        required: true,
-    },
-    Medicine: {
-        type: [String],
-        required: true,
-    }
+  PatientUsername: {
+    type: String,
+    ref: "Patient",
+    required: true,
+    trim: true,
+  },
+  TotalCost: {
+    type: Number,
+    required: true,
+  },
+  Medicine: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: "Medicine",
+  },
 });
 
 const Cart = mongoose.model("Cart", cartSchema);
