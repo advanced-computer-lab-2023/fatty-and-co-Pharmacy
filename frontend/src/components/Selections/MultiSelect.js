4a8d6510435c9a9928c0c0359da3b0347d1274be
Zeyadptd <zeyad.habashy02@gmail.com>
2023-12-01 17:12:58 +0200

    create a multiselect component using downshift

---
+++ frontend/src/components/Selections/MultiSelect.js
@@ -0,0 +1,158 @@
+import React, { useState, useMemo } from "react";
+import { useMultipleSelection, useCombobox } from "downshift";
+import {
+  Box,
+  Input,
+  Button,
+  List,
+  ListItem,
+  Tag,
+  TagLabel,
+  TagCloseButton,
+  Flex,
+} from "@chakra-ui/react";
+
+function MultiSelect({
+  initialItems,
+  initialSelectedItems,
+  onSelectedItemsChange,
+  placeholder,
+  labelKey,
+  valueKey,
+  ...rest
+}) {
+  const [inputValue, setInputValue] = useState("");
+  const [selectedItems, setSelectedItems] = useState(initialSelectedItems);
+
+  const items = useMemo(() => {
+    return initialItems.filter(
+      (item) =>
+        item[labelKey].toLowerCase().includes(inputValue.toLowerCase()) &&
+        !selectedItems.map((i) => i[labelKey]).includes(item[labelKey])
+    );
+  }, [initialItems, inputValue, selectedItems, labelKey]);
+
+  const {
+    getSelectedItemProps,
+    getDropdownProps,
+    removeSelectedItem,
+  } = useMultipleSelection({
+    selectedItems,
+    onStateChange({ selectedItems: newSelectedItems }) {
+      if (newSelectedItems) {
+        setSelectedItems(newSelectedItems);
+        onSelectedItemsChange(newSelectedItems);
+      }
+    },
+  });
+
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    highlightedIndex,
+    getItemProps,
+    selectedItem,
+  } = useCombobox({
+    items,
+    itemToString: (item) => (item ? item[labelKey] : ""),
+    inputValue,
+    onStateChange({
+      inputValue: newInputValue,
+      selectedItem: newSelectedItem,
+      type,
+    }) {
+      switch (type) {
+        case useCombobox.stateChangeTypes.InputKeyDownEnter:
+        case useCombobox.stateChangeTypes.ItemClick:
+        case useCombobox.stateChangeTypes.InputBlur:
+          if (newSelectedItem && !selectedItems.includes(newSelectedItem)) {
+            setSelectedItems([...selectedItems, newSelectedItem]);
+            setInputValue("");
+          }
+          break;
+
+        case useCombobox.stateChangeTypes.InputChange:
+          setInputValue(newInputValue);
+          break;
+        default:
+          break;
+      }
+    },
+  });
+
+  // Render component...
+  return (
+    <Box w="592px">
+      <Box d="flex" flexDirection="column" gap="1">
+        <Box
+          d="flex"
+          gap="2"
+          alignItems="center"
+          flexWrap="wrap"
+          p="1.5"
+          shadow="sm"
+          bg="white"
+        >
+          <Flex direction={"row"} wrap="wrap">
+            {selectedItems.map((selectedItem, index) => (
+              <Tag
+                key={`selected-item-${index}`}
+                borderRadius="md"
+                bg="gray.100"
+                mr="2"
+                mb="2"
+              >
+                <TagLabel>{selectedItem[labelKey]}</TagLabel>
+                <TagCloseButton
+                  onClick={() => removeSelectedItem(selectedItem)}
+                />
+              </Tag>
+            ))}
+            <Box flexShrink={0}>
+              <Input
+                placeholder={placeholder}
+                {...getInputProps(
+                  getDropdownProps({ preventKeyAction: isOpen })
+                )}
+                border="none" // Remove border from the input field
+              />
+            </Box>
+          </Flex>
+        </Box>
+      </Box>
+      <List
+        pos="absolute"
+        w="inherit"
+        bg="white"
+        mt="1"
+        shadow="md"
+        maxH="80"
+        overflow="scroll"
+        p="0"
+        zIndex="10"
+        display={isOpen && items.length ? "block" : "none"}
+        {...getMenuProps()}
+      >
+        {isOpen &&
+          items.map((item, index) => (
+            <ListItem
+              py="2"
+              px="3"
+              shadow="sm"
+              bg={highlightedIndex === index ? "blue.300" : undefined}
+              fontWeight={selectedItem === item ? "bold" : undefined}
+              key={`${item[valueKey]}${index}`}
+              {...getItemProps({ item, index })}
+            >
+              <Box>{item[labelKey]}</Box>
+            </ListItem>
+          ))}
+      </List>
+    </Box>
+  );
+}
+
+export default MultiSelect;
