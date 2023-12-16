f66f3430d4a7b05c8d92ec34637e606c48f066f3
mariamhmostafa <mariamhmaarek@gmail.com>
2023-12-16 14:57:23 +0200

    sales increases in the month of the order

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -59,7 +59,11 @@ const checkout = async (req, res) => {
       }
       medicine.Quantity -= quantity;
       medicine.Sales += quantity;
-      if(medicine.SalesPerMonth) medicine.SalesPerMonth[11]+=quantity;
+      const currentMonth = new Date().getMonth();
+      // Update the SalesPerMonth array for the current month
+      if (medicine.SalesPerMonth) {
+         medicine.SalesPerMonth[currentMonth] += quantity;
+      }
       await medicine.save();
     }
 
@@ -147,6 +151,8 @@ const cancelOrder = async (req, res) => {
     }
     // update medicine sales and quantity
     const medicines = order.Medicine;
+    const orderMonth = new Date(order.Date).getMonth();
+
     for (let i = 0; i < medicines.length; i++) {
       const medicine = await medicineModel.findOne({ _id: medicines[i] });
       const sales = medicine.Sales;
@@ -155,7 +161,9 @@ const cancelOrder = async (req, res) => {
       const newQuantity = quantity + 1;
       medicine.Sales = newSales;
       medicine.Quantity = newQuantity;
-      if(medicine.SalesPerMonth) medicine.SalesPerMonth[11]-=1;
+      if (medicine.SalesPerMonth) {
+        medicine.SalesPerMonth[orderMonth] -= 1;
+      }
       await medicine.save();
     }
 
