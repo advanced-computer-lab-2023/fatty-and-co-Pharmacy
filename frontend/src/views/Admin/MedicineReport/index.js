0c51604c20ec420cb832d06962ade163a876b067
mariamhmostafa <mariamhmaarek@gmail.com>
2023-12-16 20:21:52 +0200

    Change layout

--- frontend/src/views/Admin/MedicineReport/index.js
+++ frontend/src/views/Admin/MedicineReport/index.js
@@ -21,7 +21,7 @@ export default function MedicineReport() {
   return (
     <Flex flexDirection='column' pt={{ base: "120px", md: "75px" }}>
       <Grid
-        templateColumns={{ sm: "1fr", lg: "1.3fr 1.7fr" }}
+        templateColumns={{ sm: "1fr" }}
         templateRows={{ sm: "repeat(2, 1fr)", lg: "1fr" }}
         gap='24px'
         mb={{ lg: "26px" }}>
